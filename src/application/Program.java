ae56c5751e4e08f3905b9a4140cf6f9f34fa720b
johnnyubiali <johnnyubiali@hotmail.com>
2022-07-12 15:49:02 -0300

    List Updated and Corrections

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,7 +27,7 @@ public class Program {
 			}
 			
 			Product x = CalculationService.max(list);
-			System.out.println("Max: ");
+			System.out.println("Most Expensive: ");
 			System.out.println(x);
 		}
 		catch (IOException e){
